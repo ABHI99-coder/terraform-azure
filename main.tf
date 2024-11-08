292fad9e9ec8dc6e5fc395670ec4d679cbe7ee53
ABHI99-coder <64468233+ABHI99-coder@users.noreply.github.com>
2024-11-09 00:19:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_acc" {
-  name                     = "github_terraform"
+  name                     = "githubterraform"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
