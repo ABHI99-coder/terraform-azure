ce16091085505a1daec9ceb8e245b74a434e4688
ABHI99-coder <64468233+ABHI99-coder@users.noreply.github.com>
2024-11-08 17:57:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "github"
+  location = "Central US"
+}
+
+resource "azurerm_storage_account" "storage_acc" {
+  name                     = "github-terraform"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
